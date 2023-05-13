a7941ef2c88ee600b40cd27b692e10a58679948d
JiarongF <jiaronf1@uci.edu>
2023-05-13 19:17:42 -0400

    delete photo and select photo as cover photo

--- api/index.js
+++ api/index.js
@@ -129,7 +129,7 @@ app.post('/places', (req, res) => {
 
 });
 
-app.get('/places', (req, res) => {
+app.get('/user-places', (req, res) => {
   const {token} = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     const {id} = userData;
@@ -161,6 +161,10 @@ app.put('/places', async (req, res) => {
       placeDoc.save();
     }
   });
+})
+
+app.get('/places', async (req, res) => {
+  res.json(await Place.find());
 })
 
 app.listen(4000);
