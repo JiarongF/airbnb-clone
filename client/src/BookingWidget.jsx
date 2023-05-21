f56bfbc31923077befc1cbcbae0df8c69cf286c0
JiarongF <jiaronf1@uci.edu>
2023-05-21 18:42:21 -0400

    fixed bugs

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,15 +1,16 @@
 import {useContext, useEffect, useState} from "react";
 import {differenceInCalendarDays} from "date-fns";
 import axios from "axios";
-import {UserContext} from "./UserContext";
+import {Navigate} from "react-router-dom";
+import {UserContext} from "./UserContext.jsx";
 
 export default function BookingWidget({place}) {
-  const [checkIn, setCheckIn] = useState('');
-  const [checkOut, setCheckOut] = useState('');
-  const [numberOfGuests, setNumberOfGuests] = useState(1);
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [redirect, setRedirect] = useState('');
+  const [checkIn,setCheckIn] = useState('');
+  const [checkOut,setCheckOut] = useState('');
+  const [numberOfGuests,setNumberOfGuests] = useState(1);
+  const [name,setName] = useState('');
+  const [phone,setPhone] = useState('');
+  const [redirect,setRedirect] = useState('');
   const {user} = useContext(UserContext);
 
   useEffect(() => {
@@ -20,29 +21,27 @@ export default function BookingWidget({place}) {
 
   let numberOfNights = 0;
   if (checkIn && checkOut) {
-    numberOfNights = differenceInCalendarDays(new Date(checkOut),new Date(checkIn));
+    numberOfNights = differenceInCalendarDays(new Date(checkOut), new Date(checkIn));
   }
 
-
   async function bookThisPlace() {
-
-    const response = await axios.post('/bookings', {checkIn, checkOut, numberOfGuests, name, phone,
-      place: place.id,
-      price: numberOfNights * place.price,});
-
+    const response = await axios.post('/bookings', {
+      checkIn,checkOut,numberOfGuests,name,phone,
+      place:place._id,
+      price:numberOfNights * place.price,
+    });
     const bookingId = response.data._id;
-    setRedirect(`/account/bookings/${bookingId}`)
+    setRedirect(`/account/bookings/${bookingId}`);
   }
 
   if (redirect) {
-    return <Navigate to = {redirect}/>
+    return <Navigate to={redirect} />
   }
-
 
   return (
       <div className="bg-white shadow p-4 rounded-2xl">
         <div className="text-2xl text-center">
-          Price: ${place.price} /per night
+          Price: ${place.price} / per night
         </div>
         <div className="border rounded-2xl mt-4">
           <div className="flex">
@@ -54,8 +53,7 @@ export default function BookingWidget({place}) {
             </div>
             <div className="py-3 px-4 border-l">
               <label>Check out:</label>
-              <input type="date"
-                     value={checkOut}
+              <input type="date" value={checkOut}
                      onChange={ev => setCheckOut(ev.target.value)}/>
             </div>
           </div>
@@ -81,9 +79,8 @@ export default function BookingWidget({place}) {
         <button onClick={bookThisPlace} className="primary mt-4">
           Book this place
           {numberOfNights > 0 && (
-              <span> ${numberOfNights * place.price} </span>
+              <span> ${numberOfNights * place.price}</span>
           )}
-
         </button>
       </div>
   );
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,20 +1,13 @@
 import {useContext, useState} from "react";
-import {UserContext} from "../UserContext";
+import {UserContext} from "../UserContext.jsx";
 import {Link, Navigate, useParams} from "react-router-dom";
 import axios from "axios";
 import PlacesPage from "./PlacesPage";
 import AccountNav from "../AccountNav";
 
 export default function ProfilePage() {
-  const [redirect, setRedirect] = useState(null);
+  const [redirect,setRedirect] = useState(null);
   const {ready,user,setUser} = useContext(UserContext);
-  if (!ready) {
-    return 'Loading...';
-  }
-  if (ready && !user && !redirect) {
-    return <Navigate to = {'/login'} />
-  }
-
   let {subpage} = useParams();
   if (subpage === undefined) {
     subpage = 'profile';
@@ -26,20 +19,27 @@ export default function ProfilePage() {
     setUser(null);
   }
 
-  if (redirect) {
-    return <Navigate to = {redirect} />
+  if (!ready) {
+    return 'Loading...';
   }
 
-  return(
+  if (ready && !user && !redirect) {
+    return <Navigate to={'/login'} />
+  }
+
+  if (redirect) {
+    return <Navigate to={redirect} />
+  }
+  return (
       <div>
-        <AccountNav/>
+        <AccountNav />
         {subpage === 'profile' && (
             <div className="text-center max-w-lg mx-auto">
-              Logged in as {user.name} ({user.email}) <br />
+              Logged in as {user.name} ({user.email})<br />
               <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
             </div>
         )}
-        {subpage === 'place' && (
+        {subpage === 'places' && (
             <PlacesPage />
         )}
       </div>
